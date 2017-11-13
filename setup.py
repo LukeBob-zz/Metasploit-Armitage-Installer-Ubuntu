fea0c30beb9d14bfb09e87a5c92e2244a8fc0711
Luke <bob_monday@outlook.com>
2017-11-13 04:31:28 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import time
 Cur_Dir = __file__
 
 def Install_Java():
-    print("\n\t Installing Java\n\n")
+    print("\n\t [#] Installing Java [#]\n\n")
     time.sleep(2)
     repo = """
     add-apt-repository -y ppa:webupd8team/java
@@ -18,11 +18,11 @@ def Install_Java():
     apt-get -y install oracle-java8-installer
     """
     os.system(repo)
-    print("\n\t Java Installed\n\n")
+    print("\n\t [#] Java Installed [#]\n\n")
     time.sleep(2)    
 
 def Install_Dependencys():
-    print("\n\t Installing Dependencys\n\n")
+    print("\n\t [#] Installing Dependencys [#]\n\n")
     time.sleep(2)
     Deps = """
     apt-get update
@@ -30,11 +30,11 @@ def Install_Dependencys():
     apt-get -y install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev vncviewer libyaml-dev curl zlib1g-dev
     """
     os.system(Deps)
-    print("\n\t Dependencys Installed\n\n")
+    print("\n\t [#] Dependencys Installed [#]\n\n")
     time.sleep(2)
 
 def Install_Ruby():
-    print("\n\t Installing Ruby\n\n")
+    print("\n\t [#] Installing Ruby [#]\n\n")
     time.sleep(2)
     Rub = """
     curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
@@ -52,11 +52,11 @@ def Install_Ruby():
     os.system(Rub)
     time.sleep(1) ## Debugging purposes
     os.system(Rub1)
-    print("\n\tRuby Installed\n")
+    print("\n\t [#] Ruby Installed [#]\n\n")
     time.sleep(2)
 
 def Install_Nmap():
-    print("\n\tInstalling Nmap\n\n")
+    print("\n\t [#] Installing Nmap [#]\n\n")
     time.sleep(2)
     nm = """
     mkdir ~/Development
@@ -70,11 +70,11 @@ def Install_Nmap():
     """
 
     os.system(nm)
-    print("\n\tInstalled Nmap\n\n")
+    print("\n\t [#] Installed Nmap [#]\n\n")
     time.sleep(2)
 
 def Configure_Postgresql():
-    print("\n\t Configuring Postgresql For Metasploit\n\n")
+    print("\n\t [#] Configuring Postgresql For Metasploit [#]\n\n")
     time.sleep(2)
     post = """
     su postgres
@@ -83,11 +83,11 @@ def Configure_Postgresql():
     """
 
     os.system(post)
-    print("\nPostgresql Configured\n\n")
+    print("\n\t [#] Postgresql Configured [#]\n\n")
     time.sleep(2)
 
 def Install_Metasploit():
-    print("\n\tInstalling Metasploit Framework\n\n")
+    print("\n\t [#] Installing Metasploit Framework [#]\n\n")
     time.sleep(2)
     meta = """
     cd /opt
@@ -101,12 +101,12 @@ def Install_Metasploit():
     """
 
     os.system(meta)
-    print("\n\t Installed Metasploit Framework\n\n")
+    print("\n\t [#] Installed Metasploit Framework [#]\n\n")
     time.sleep(2)
 
 
 def Install_Armitage():
-    print("\n\t Installing Armitage\n\n")
+    print("\n\t [#] Installing Armitage [#]\n\n")
     time.sleep(2)
     armit = """
     curl -# -o /tmp/armitage.tgz http://www.fastandeasyhacking.com/download/armitage150813.tgz
@@ -128,7 +128,7 @@ def Install_Armitage():
     """
     os.system(chdir)
     os.system(cet)
-    print("\n\t Armitage Installed\n")
+    print("\n\t [#] Armitage Installed [#]\n\n")
     time.sleep(2)
 
 def main():
