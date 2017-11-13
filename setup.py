e8ba4b2c6631b9a04c23ac28928875736a5be17d
Luke <bob_monday@outlook.com>
2017-11-13 02:26:42 +0000

    Create setup.py
    installs all dependencys, metasploit then armitage. A system reboot is required after instilation.

---
+++ setup.py
@@ -0,0 +1,134 @@
+import os
+import time
+
+Cur_Dir = __file__
+
+def Install_Java():
+    print("\n\t Installing Java\n")
+    repo = """
+    add-apt-repository -y ppa:webupd8team/java
+    apt-get update
+    apt-get -y install oracle-java8-installer
+    """
+    os.system(repo)
+    print("\n\t Java Installed\n")
+
+def Install_Dependencys():
+    print("\n\t Installing Dependencys")
+    Deps = """
+    apt-get update
+    apt-get upgrade
+    apt-get install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev vncviewer libyaml-dev curl zlib1g-dev
+    """
+    os.system(Deps)
+    print("\n\t Dependencys Installed")
+
+def Install_Ruby():
+    print("\n\t Installing Ruby\n")
+    Rub = """
+    cd ~/
+    curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+    curl -L https://get.rvm.io | bash -s stable
+    source ~/.rvm/scripts/rvm
+    echo 'source ~/.rvm/scripts/rvm' >> ~/.bashrc
+    source ~/.bashrc
+    RUBYVERSION=$(wget https://raw.githubusercontent.com/rapid7/metasploit-framework/master/.ruby-version -q -O - )
+    rvm install $RUBYVERSION
+    rvm use $RUBYVERSION --default
+    ruby -v
+    """
+    os.system(rub)
+    print("\n\tRuby Installed\n")
+
+def Install_Nmap():
+    print("\n\tInstalling Nmap\n")
+    nm = """
+    mkdir ~/Development
+    cd ~/Development
+    git clone https://github.com/nmap/nmap.git
+    cd nmap
+    ./configure
+    make
+    make install
+    make clean
+    """
+
+    os.system(nm)
+    print("\n\tInstalled Nmap\n")
+
+def Configure_Postgresql():
+    print("\n\t Configuring Postgressql For Metasploit\n")
+    post = """
+    sudo -s
+    su postgres
+    createuser msf -P -S -R -D
+    createdb -O msf msf
+    exit
+    exit
+    """
+
+    os.system(post)
+    print("\nPostgresql Configured\n")
+
+def Install_Metasploit():
+    print("\n\tInstalling Metasploit Framework\n")
+    meta = """
+    cd /opt
+    git clone https://github.com/rapid7/metasploit-framework.git
+    chown -R root /opt/metasploit-framework
+    cd metasploit-framework
+    rvm --default use ruby-${RUBYVERSION}@metasploit-framework
+    gem install bundler
+    bundle install
+    bash -c 'for MSF in $(ls msf*); do ln -s /opt/metasploit-framework/$MSF /usr/local/bin/$MSF;done'
+    """
+
+    os.system(meta)
+    print("\n\t Installed Metasploit Framework\n")
+
+
+def Install_Armitage():
+    print("\n\t Installing Armitage")
+    armit = """
+    curl -# -o /tmp/armitage.tgz http://www.fastandeasyhacking.com/download/armitage150813.tgz
+    tar -xvzf /tmp/armitage.tgz -C /opt
+    ln -s /opt/armitage/armitage /usr/local/bin/armitage
+    ln -s /opt/armitage/teamserver /usr/local/bin/teamserver
+    sh -c 'echo java -jar /opt/armitage/armitage.jar \$\* > /opt/armitage/armitage'
+    perl -pi -e 's/armitage.jar/\/opt\/armitage\/armitage.jar/g' /opt/armitage/teamserver
+    touch /opt/metasploit-framework/config/database.yml
+    """
+    os.system(armit)
+    dir_path = os.path.dirname(os.realpath(Cur_Dir))
+    chdir = "cd {0}".format(dir_path)
+
+    cet   = """
+    cat database.yml > /opt/metasploit-framework/config/database.yml
+    sh -c 'echo export MSF_DATABASE_CONFIG=/opt/metasploit-framework/config/database.yml >> /etc/profile'
+    source /etc/profile
+    """
+    os.system(chdir)
+    os.system(cet)
+    print("\n\t Armitage Installed\n")
+
+def main():
+    try:
+        Install_Java()
+        Install_Dependencys()
+        Install_Ruby()
+        Install_Nmap()
+        Configure_Postgresql()
+        Install_Metasploit()
+        Install_Armitage()
+    except: raise
+    exit(0)
+
+    os.system("msfconsole")
+    time.sleep(5)
+    os.system("exit")
+    print("\n\tDefault Armitage User:msf Pass:msf\n\n")
+    print("\n\t Metasploit-Framework\n\t Successfully Installed\n\t System Needs To Be Rebooted\n")
+
+
+if __name__ == '__main__':
+    main()
